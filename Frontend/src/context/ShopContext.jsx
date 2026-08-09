4e61fdef7842f631c39051e209876aaa727f21bc
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-08-09 23:01:50 +0530

    Added collection page related products feature

--- Frontend/src/context/ShopContext.jsx
+++ Frontend/src/context/ShopContext.jsx
@@ -1,5 +1,7 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import { products } from "../assets/frontend_assets/assets";
+import { useEffect } from "react";
+import { toast } from "react-toastify";
 
 
 
@@ -8,7 +10,62 @@ export const ShopContext= createContext()
 const ShopContextProvider= (props)=>{
   const currency="$"
   const delivery_fee=10
-  const value= {products, currency, delivery_fee}
+  const [search, setSearch]= useState("")
+  const [showSearch, setShowSearch]= useState(false)
+  const [cartItems, setCartItems]= useState({})
+
+  async function addToCart(itemId, size)
+  {
+
+    if(!size)
+    {
+      toast.error("Select Product Size")
+      return
+    }
+
+      let cartData= structuredClone(cartItems)
+      if(cartData[itemId])
+      {
+        if(cartData[itemId][size])
+        {
+          cartData[itemId][size]+=1
+        }
+        else
+        {
+          cartData[itemId][size]=1
+        }
+      }
+      else
+      {
+        cartData[itemId]={}
+        cartData[itemId][size]=1
+      }
+      setCartItems(cartData)
+  }
+
+  function getCartCount()
+  {
+    let totalCount=0
+    for(const items in cartItems)
+    {
+      for(const item in cartItems[items])
+      {
+        try{
+              if(cartItems[items][item]>0)
+              {
+                totalCount+=cartItems[items][item]
+              }
+        }
+        catch(error)
+        {
+
+        }
+      }
+    }
+    return totalCount
+  }
+
+  const value= {products, currency, delivery_fee, search, setSearch, showSearch, setShowSearch, cartItems, addToCart, getCartCount}
 
   return (
     <ShopContext.Provider value={value}>
