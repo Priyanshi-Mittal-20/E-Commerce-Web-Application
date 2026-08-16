e3c2f5ec6071cef748664e042f0f55b1843320f2
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-08-16 15:22:18 +0530

    Backend setup completed

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,16 @@
+async function loginUser(req, res)
+{
+
+}
+
+async function registerUser(req, res)
+{
+  
+}
+
+async function adminLogin(req, res)
+{
+  
+}
+
+export {loginUser, registerUser, adminLogin}
