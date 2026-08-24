baf3bf0421b9c6b13c2e76cf732b66efa041b7d7
Priyanshi Mittal <priyanshimzn05@gmail.com>
2026-08-24 12:46:21 +0530

    Setup of admin panel, admin panel navbar and sidebar components completed.

---
+++ Admin/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {assets} from "../assets/assets"
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center py-2 px-[4%] justify-between'>
+      <img src={assets.logo} alt="ShopSphere" className="w-32 sm:w-36" />
+      <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
+    </div>
+  )
+}
+
+export default Navbar
